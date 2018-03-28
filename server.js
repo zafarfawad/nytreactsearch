abe1206bbe6b47e14c76deed0ff40d8f3b1d27c6
Fawad Zafar <fawad.zafar@robotsandpencils.com>
2018-03-27 21:06:44 -0400

    heroku fixing

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  process.env.MONGOLAB_ORANGE_URI || "mongodb://localhost/nyctimesarticles",
   {
     // useMongoClient: true
   }
